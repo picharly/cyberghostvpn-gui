267e881f4304ae8f7fe2637f094e2b524bdde8d2
picharly <160637551+picharly@users.noreply.github.com>
2024-11-25 09:42:48 -0500

    Add translations (thanks ChatGPT) and fix profile box translation issue

--- about/app.go
+++ about/app.go
@@ -4,5 +4,5 @@ package about
 const AppDescr = "Simple CyberGhost VPN GUI for Linux - by C.Pichard"
 const AppID = "com.github.picharly.cyberghostvpn-gui"
 const AppName = "CyberGhost VPN GUI"
-const AppVersion = "0.9.2"
+const AppVersion = "0.9.9"
 const DevelopmentMode = true
