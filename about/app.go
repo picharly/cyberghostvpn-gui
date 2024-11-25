486dd9c5a48fe734f8268e93f13476edcdf4534d
picharly <160637551+picharly@users.noreply.github.com>
2024-11-25 09:44:40 -0500

    Update version (first stable) and add password focus

--- about/app.go
+++ about/app.go
@@ -4,5 +4,5 @@ package about
 const AppDescr = "Simple CyberGhost VPN GUI for Linux - by C.Pichard"
 const AppID = "com.github.picharly.cyberghostvpn-gui"
 const AppName = "CyberGhost VPN GUI"
-const AppVersion = "0.9.9"
+const AppVersion = "1.0.0"
 const DevelopmentMode = true
